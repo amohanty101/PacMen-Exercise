90ddfff7a7232f6dde42136a265085491ed992b1
Anjali Mohanty <anjali.mohanty101@gmail.com>
2022-12-23 10:06:37 -0600

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['./PacMan1.png', './PacMan2.png'],
+  ['./PacMan3.png', './PacMan4.png'],
 ];
 var direction = 0;
 const pacMen = [];
