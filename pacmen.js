01e032343531777ad004b2f092ed4648c8d26b0f
Anjali Mohanty <anjali.mohanty101@gmail.com>
2022-12-23 10:16:13 -0600

    Add files via upload
    javascript file uploaded with correct image path file updated with the right path for images
    uploaded an images folder

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-  ['./PacMan1.png', './PacMan2.png'],
-  ['./PacMan3.png', './PacMan4.png'],
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
 var direction = 0;
 const pacMen = [];
